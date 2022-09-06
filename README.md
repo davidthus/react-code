16492c5e4e729bc38374646d6fb041af38456d07
davidthus <dvdnicorici05@gmail.com>
2022-09-06 21:43:59 +0100

    finish intro to useRef

---
+++ README.md
@@ -0,0 +1,9 @@
+# useRef
+
+useRef is a hook which can be used to store a value like state (which persists over renders) but does not update state when it changes.
+
+It can be used to
+
+1. Store a DOM node
+2. Store the previous value of the state.
+3. Store any value that persists over renders but does not re-render the component
--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const [name, setName] = useState('')
+  const prevName = useRef('')
+  const input = useRef(null);
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name])
+
   return (
     <div className="App">
-      
+      <input type="text" ref={input} value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name}</div>
+      <div>And my name used to be {prevName.current}</div>
     </div>
   );
 }
